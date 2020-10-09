bb6d0481f3b36d209fd8ef22b6134529e460ea83
Emery <epmarks@ymail.com>
2020-10-09 17:59:50 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+  return a + b;
+}
+
+sum = add(4, 6);
+console.log(sum);
+
+function multiply(n) {
+  for (i = 0, i < 4; i++) {
+      add(n)
+  }
+  return n
+  }
+
+
+
+// multiple = multiply(4, 6);
+// console.log(multiple);
+
+// function power(x, n) {
+//   return multiply(x, n);
+// }
+
+// powerOf = power(2, 8);
+// console.log(powerOf);
