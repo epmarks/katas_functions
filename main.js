1021ef8f9c3a6f8b6b8f793062117f0e86141e1c
Emery <epmarks@ymail.com>
2020-10-16 17:32:35 -0400

    add through factorial complete. webpage includes inputs

--- main.js
+++ main.js
@@ -1,25 +1,111 @@
+let addNum1 = document.querySelector("#add1");
+let addNum2 = document.querySelector("#add2");
+let multNum1 = document.querySelector("#mult1");
+let multNum2 = document.querySelector("#mult2");
+let powNum1 = document.querySelector("#pow1");
+let powNum2 = document.querySelector("#pow2");
+let factNum = document.querySelector("#fact");
+let fiboNum = document.querySelector("#fibo");
+
+let addButton = document.querySelector("#add-button");
+let multiplyButton = document.querySelector("#multiply-button");
+let powerButton = document.querySelector("#power-button");
+let factorButton = document.querySelector("#factor-button");
+let resetButton = document.querySelector("#reset-button");
+let fibonacciButton = document.querySelector("#fibonacci-button");
+
+let addTotal = document.querySelector("#add-total");
+let multiplyTotal = document.querySelector("#multiply-total");
+let powerTotal = document.querySelector("#power-total");
+let factorTotal = document.querySelector("#factor-total");
+let selectedNumber = document.querySelector("#selected-number");
+
 function add(a, b) {
   return a + b;
 }
 
-sum = add(4, 6);
-console.log(sum);
+function multiply(a, b) {
+  let count = 0;
+  let answer = 0;
 
-// function multiply(n) {
-//   let i = 0;
-//   while (i < n) {
-//     add(num);
-//     i++;
+  while (count < b) {
+    answer = add(a, answer);
+    count += 1;
+  }
+  return answer;
+}
+
+function power(a, b) {
+  let count = 0;
+  let answer = 1;
+
+  while (count < b) {
+    answer = multiply(a, answer);
+    count += 1;
+  }
+  return answer;
+}
+
+// I was totally lost on factorial, I used this website as a reference https://www.freecodecamp.org/news/how-to-factorialize-a-number-in-javascript-9263c89a4b38/
+
+function factor(a) {
+  let count = 0;
+  let answer = a;
+
+  if (a === 0 || a === 1) return 1;
+
+  while (a > 1) {
+    a--;
+    answer = multiply(answer, a);
+  }
+  return answer;
+}
+
+// function fibonacci(a) {
+//   let fibocacciArray = [0, 1, 1, 2, 3, 5, 8, 13, 21];
+//   let count = 0;
+
+//   while (count < fibocacciArray.length) {
+//     a = fibocacciArray.indexOf(a);
 //   }
-//   return num;
+//   return a;
 // }
 
-// multiple = multiply(4, 6);
-// console.log(multiple);
+addButton.addEventListener("click", function () {
+  let addition = add(Number(addNum1.value), Number(addNum2.value));
+  addTotal.innerHTML = addition;
+});
 
-// function power(x, n) {
-//   return multiply(x, n);
-// }
+multiplyButton.addEventListener("click", function () {
+  let multiplication = multiply(Number(multNum1.value), Number(multNum2.value));
+  multiplyTotal.innerHTML = multiplication;
+});
 
-// powerOf = power(2, 8);
-// console.log(powerOf);
+powerButton.addEventListener("click", function () {
+  let exponentiation = power(Number(powNum1.value), Number(powNum2.value));
+  powerTotal.innerHTML = exponentiation;
+});
+
+factorButton.addEventListener("click", function () {
+  let factorial = factor(Number(factNum.value));
+  factorTotal.innerHTML = factorial;
+});
+
+// fibonacciButton.addEventListener("click", function () {
+//   let fibo = fibonacci(Number(fiboNum.value));
+//   selectedNumber.innerHTML = fibo;
+// });
+
+resetButton.addEventListener("click", function () {
+  addNum1.value = "";
+  addNum2.value = "";
+  multNum1.value = "";
+  multNum2.value = "";
+  powNum1.value = "";
+  powNum2.value = "";
+  factNum.value = "";
+  addTotal.innerHTML = "";
+  multiplyTotal.innerHTML = "";
+  powerTotal.innerHTML = "";
+  factorTotal.innerHTML = "";
+});
